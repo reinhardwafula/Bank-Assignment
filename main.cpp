87689c300d89020d68f79fbb8b238523e02c3d08
reinhardwafula <rehynsanders@gmail.com>
2021-11-18 14:02:36 +0300

    Implemented function 'enterAccountData()'

--- main.cpp
+++ main.cpp
@@ -2,8 +2,31 @@
 
 using namespace std;
 
+class Bank{
+    static const int interestRate = 0.03;
+    private:
+        int accNumber;
+        int accBalance;
+    public:
+        void enterAccountData(){
+            cout << "Enter account number: ";
+            cin >> accNumber;
+            while(accNumber < 1000){
+                cout << "Enter a valid account number(value greater than 1000): ";
+                cin >> accNumber;
+            }
+            cout << "Enter account balance: ";
+            cin >> accBalance;
+            while(accBalance < 0){
+                cout << "Enter a valid account balance(a positve a value): ";
+                cin >> accBalance;
+            }
+        }
+};
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    Bank user1;
+    user1.enterAccountData();
     return 0;
 }
